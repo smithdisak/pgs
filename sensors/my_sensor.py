3ece16921036e769260f0e317a9852e175b37e78
smithdisak thanyachatthatri <smithdisakthanyachatthatri@smithdisaks-iMac.local>
2021-05-02 15:30:19 +0100

    add sub dir

---
+++ sensors/my_sensor.py
@@ -0,0 +1,16 @@
+from airflow.sensors.base_sensor_operator import BaseSensorOperator
+#from airflow.sensors import BaseSensorOperator
+#from airflow.operators import BashOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class MySensor(BaseSensorOperator):
+
+    @apply_defaults
+    def __init__(self,
+                 *args,
+                 **kwargs):
+        super(MySensor, self).__init__(*args, **kwargs)
+
+    def poke(self, context):
+        return True
